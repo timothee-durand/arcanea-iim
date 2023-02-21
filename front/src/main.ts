1cf02377575494ebcd715f40412f088eb96f3fcb
Jessy <ernatusjessy@gmail.com>
2023-02-21 15:01:36 +0100

    add front login to room

--- front/src/main.ts
+++ front/src/main.ts
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import { Socket } from './plugins/socket'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(Socket)
+app.mount('#app')
---
+++ front/src/plugins/socket.ts
@@ -0,0 +1,18 @@
+import { io } from "socket.io-client";
+import type { Plugin } from "vue";
+
+export const Socket: Plugin = {
+  install(app) {
+    const socket = io("http://localhost:8080");
+
+    socket.on("connect", () => {
+      console.log("Connected to socket");
+    });
+
+    socket.on("disconnect", () => {
+      console.log("Disconnected from socket");
+    });
+
+    app.provide("socket", socket);
+  },
+};
