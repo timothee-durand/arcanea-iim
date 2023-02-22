f1600edf477b2e9049d7e10b8ca07bc5b4beb800
Adrien <adrien.bouteiller01@gmail.com>
2023-02-22 16:05:29 +0100

    finish page deck

--- front/src/components/HelloWorld.vue
+++ front/src/components/HelloWorld.vue
@@ -20,6 +20,7 @@ export default {
 
   mounted() {
     this.fetchCards();
+
   },
 
   methods: {
@@ -37,9 +38,10 @@ export default {
       const response = await fetch("https://ktyxaunussvaacscgpon.supabase.co/rest/v1/Cards", requestOptions)
       this.result = await response.json()
 
-      const back = await fetch("https://ktyxaunussvaacscgpon.supabase.co/rest/v1/Cards?name=eq.Lumos", requestOptions)
+      const back = await fetch("https://ktyxaunussvaacscgpon.supabase.co/rest/v1/Cards?name=eq.back", requestOptions)
       this.resultBack = await back.json()
       console.log(this.resultBack)
+      let toto = document.querySelector('.BACK').style.display = 'none'
 
     },
 
@@ -60,7 +62,7 @@ export default {
 
 
       var imageBack = new Image();
-      imageBack.src = this.result[0].image;
+      imageBack.src = this.resultBack[0].image;
       var textureBack = new THREE.Texture();
       textureBack.image = imageBack;
       imageBack.onload = function () {
@@ -87,9 +89,13 @@ export default {
       var material2 = new THREE.MeshLambertMaterial({ map: textureBack });
 
       const plane1 = new THREE.Mesh(geometry1, material1);
-      scene.add(plane1);
       const plane2 = new THREE.Mesh(geometry2, material2);
-      scene.add(plane2);
+
+      const group = new THREE.Group();
+      group.add(plane1);
+      group.add(plane2);
+
+      scene.add(group);
 
 
 
@@ -117,14 +123,14 @@ export default {
 
       };
 
-
-
       render();
 
       const controls = new OrbitControls(camera, renderer.domElement);
 
+
       //controls.update() must be called after any manual changes to the camera's transform
       camera.position.set(0, 0, 1);
+      controls.autoRotate = true;
       controls.update();
 
       function animate() {
@@ -132,11 +138,13 @@ export default {
         requestAnimationFrame(animate);
 
         // required if controls.enableDamping or controls.autoRotate are set to true
+
+
         controls.update();
-
         renderer.render(scene, camera);
-
       }
+
+      animate()
     }
   }
 }
@@ -144,16 +152,28 @@ export default {
 
 <template>
   <div class="deckPage">
-    <div class="cross" v-show="canvas"></div>
-    <canvas ref="canvas" v-show="canvas">
-    </canvas>
-    <div class="deck">
-      <div class="cards" v-for="item in result" :key="item.result">
-        <div v-if="item.name !== 'back'">
-          <img :src="item.image" :alt="item.name" @click="Image(item.image)">
+    <div class="allCards">
+      <div class="cross" v-show="canvas" @click="this.canvas = false"></div>
+      <div class="blur" v-show="canvas"></div>
+      <canvas ref="canvas" v-show="canvas">
+      </canvas>
+      <div class="deck">
+        <div class="cards" v-for="item in result" :key="item.result" :class="item.key">
+          <div v-if="item.name !== 'back'">
+            <img :src="item.image" :alt="item.name" @click="Image(item.image)">
+          </div>
         </div>
       </div>
-
+    </div>
+    <div class="info">
+      <img src="/src/assets/img/Arcanea-logo.png" alt="logo arcanea">
+      <p>Découvrez les différentes cartes de sorts utilisables en jeu&nbsp;!
+        <br /><br />
+        Pour découvrir un sort et visualiser sa carte en 3D, vous pouvez cliquer dessus.
+        <br /><br />
+        Lors de la partie, votre main sera toujours composée de 3 cartes attribuées aléatoirement. Le jeu se terminera
+        lorsqu'un des deux joueurs atteindra 0 HP.
+      </p>
     </div>
   </div>
 </template>
@@ -167,39 +187,91 @@ export default {
   background: linear-gradient(180deg, rgba(0, 0, 0, 0.5718662464985995) 0%, rgba(19, 35, 59, 1) 100%);
   background-repeat: no-repeat;
   background-size: cover;
-  height: 100vh
+  display: flex;
+  justify-content: space-between;
+
+  .allCards {
+    width: 80%;
+    margin: 20px 0;
+
+    .blur {
+      width: 100%;
+      height: 100%;
+      position: fixed;
+      top: 0;
+      left: 0;
+      background: rgba(0, 0, 0, 0.2); // Make sure this color has an opacity of less than 1
+      backdrop-filter: blur(8px); // This be the blur
+      z-index: 8;
+    }
+  }
+
+  .info {
+    position: fixed;
+    height: 100vh;
+    right: 0;
+    width: 20%;
+    background-color: #000000;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    text-align: left;
+    padding: 56px 0;
+
+    .game {
+      position: absolute;
+      bottom: 56px;
+      color: #FFFFFF;
+      background-color: #514A92;
+      padding: 10px 35px;
+      border-radius: 50px
+    }
+
+    img {
+      width: 104px;
+      padding-bottom: 56px
+    }
+
+    p {
+      color: #FFFFFF;
+      width: 100%;
+      padding: 0 40px
+    }
+  }
 }
 
 .cross {
-  position: absolute;
+  position: fixed;
+  background-image: url('/src/assets/img/close.svg');
   background-repeat: no-repeat;
   background-size: cover;
   width: 50px;
   aspect-ratio: 1/1;
   top: 15px;
-  right: 15px
+  right: 15px;
+  z-index: 10;
+  cursor: pointer
 }
 
 
 canvas {
-  position: absolute;
+  position: fixed;
   top: 0;
   left: 0;
   width: 50%;
-
-
+  z-index: 9;
 }
 
 .deck {
   display: flex;
-  justify-content: center;
+  justify-content: space-between;
+  padding: 20px 65px;
+  flex-wrap: wrap;
   gap: 5%;
 
   .cards {
     img {
       width: 250px;
-
-
     }
   }
 
