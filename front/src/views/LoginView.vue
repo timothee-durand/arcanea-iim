32e64d3d34e9f794e07a951b911de37dc2bd290d
Adrien <adrien.bouteiller01@gmail.com>
2023-02-23 00:20:07 +0100

    add card in page login

--- front/src/views/LoginView.vue
+++ front/src/views/LoginView.vue
@@ -5,146 +5,148 @@ import { useToast } from "vue-toastification";
 import { useAuthStore } from "@/store/auth";
 import { useRouter } from "vue-router";
 import { GAME_ROUTE_NAME } from "@/router";
-import cardsCascades from '../assets/cardsCascades.png'
-import arcaneaLogo from '../assets/arcaneaLogo.png'
-import FloatCards from '../components/FloatCards.vue'
+import cardsCascades from "../assets/cardsCascades.png";
+import arcaneaLogo from "../assets/arcaneaLogo.png";
+import FloatCards from "../components/FloatCards.vue";
 
 const idRoom = ref<String>("");
 const userName = ref<String>("");
 const password = ref<String>("");
 const store = useAuthStore();
-const toast = useToast()
-const router = useRouter()
+const toast = useToast();
+const router = useRouter();
 
 const socket: Socket = inject("socket") as Socket;
 
 socket.on("userJoined", (payload) => {
-    console.log(payload, payload.duel.roomId, payload.userId, payload.duel)
-    store.roomId = payload.duel.roomId
-    store.user = payload.user
-    store.room = payload.duel
-    toast.success("You joined the room")
-    router.push({ name: GAME_ROUTE_NAME })
-})
+  console.log(payload, payload.duel.roomId, payload.userId, payload.duel);
+  store.roomId = payload.duel.roomId;
+  store.user = payload.user;
+  store.room = payload.duel;
+  toast.success("You joined the room");
+  router.push({ name: GAME_ROUTE_NAME });
+});
 
 socket.on("joinRoom", (rooms: String, user: String) => {
-    console.log("join", rooms, user)
-})
-
+  console.log("join", rooms, user);
+});
 
 socket.on("roomFull", () => {
-    console.log("roomFull")
-    toast.error("Room is full")
-})
-
+  console.log("roomFull");
+  toast.error("Room is full");
+});
 
 const joinRoom = () => {
-    if (0 !== userName.value.length) {
-        socket.emit("joinRoom", idRoom.value, userName.value)
-    }
-}
-
+  if (0 !== userName.value.length) {
+    socket.emit("joinRoom", idRoom.value, userName.value);
+  }
+};
 </script>
 <template>
-    <div class="container">
-        <form @submit="joinRoom" class="formContainer">
-            <h1>Connexion</h1>
-            <div class="spaceBetweenTerms" />
-            <span class="spaceBetweenTerms">
-                <p>Connectez-vous à votre compte pour jouer</p>
-            </span>
-            <div class="formConnexion">
-                <input class="inputCustom" placeholder="PSEUDO" v-model="userName" />
-                <input class="inputCustom" placeholder="MOT DE PASSE" v-model="password" />
-                <input class="inputCustom" placeholder="ROOM" v-model="idRoom" />
-                <input type="submit" value="CONNEXION" class="connexionButton" />
-            </div>
-        </form>
-        <FloatCards />
-        <div class="logoRow">
-            <img class="imgLogo" :src="arcaneaLogo" />
-        </div>
+  <div class="container">
+    <form @submit="joinRoom" class="formContainer">
+      <h1>Connexion</h1>
+      <div class="spaceBetweenTerms" />
+      <span class="spaceBetweenTerms">
+        <p>Connectez-vous à votre compte pour jouer</p>
+      </span>
+      <div class="formConnexion">
+        <input class="inputCustom" placeholder="PSEUDO" v-model="userName" />
+        <input
+          class="inputCustom"
+          placeholder="MOT DE PASSE"
+          v-model="password"
+        />
+        <input class="inputCustom" placeholder="ROOM" v-model="idRoom" />
+        <input type="submit" value="CONNEXION" class="connexionButton" />
+      </div>
+    </form>
+    <FloatCards />
+    <div class="logoRow">
+      <img class="imgLogo" :src="arcaneaLogo" />
     </div>
+  </div>
 </template>
 <style scoped lang="scss">
 .container {
-    position: relative;
-    max-height: 100vh;
+  position: relative;
+  max-height: 100vh;
+  overflow: hidden;
 
-    .formContainer {
-        display: flex;
-        position: fixed;
-        left: 7%;
-        bottom: 30%;
-        display: inline;
+  .formContainer {
+    display: flex;
+    position: fixed;
+    left: 7%;
+    bottom: 30%;
+    display: inline;
+    color: #fff;
+
+    .spaceBetweenTerms {
+      margin: 20px;
+    }
+
+    h1 {
+      font-family: "BluuNext-Bold";
+      font-size: 34px;
+    }
+
+    span {
+      font-size: 18px;
+      font-family: "Poppins-ExtraLight";
+    }
+
+    .formConnexion {
+      color: #fff;
+      width: 290px;
+
+      .connexionButton {
+        background-color: #514a92;
+        border-radius: 50px;
         color: #fff;
+        height: 50px;
+        width: 100%;
+        border: none;
+        font-size: 20px;
+      }
 
-        .spaceBetweenTerms {
-            margin: 20px;
-        }
+      .inputCustom {
+        width: 100%;
+        padding: 10px 0;
+        font-size: 16px;
+        font-family: "Poppins-Regular";
+        margin-bottom: 30px;
+        border: none;
+        border-bottom: 1px solid #fff;
+        outline: none;
+        background: transparent;
+        color: #fff;
+      }
 
-        h1 {
-            font-family: 'BluuNext-Bold';
-            font-size: 34px;
-        }
-
-        span {
-            font-size: 18px;
-            font-family: "Poppins-ExtraLight";
-        }
-
-        .formConnexion {
-            color: #fff;
-            width: 290px;
-
-            .connexionButton {
-                background-color: #514A92;
-                border-radius: 50px;
-                color: #fff;
-                height: 50px;
-                width: 100%;
-                border: none;
-                font-size: 20px;
-            }
-
-            .inputCustom {
-                width: 100%;
-                padding: 10px 0;
-                font-size: 16px;
-                font-family: "Poppins-Regular";
-                margin-bottom: 30px;
-                border: none;
-                border-bottom: 1px solid #fff;
-                outline: none;
-                background: transparent;
-                color: #fff;
-            }
-
-            .inputCustom::-webkit-input-placeholder {
-                color: #fff;
-                font-family: "Poppins-ExtraLight";
-            }
-        }
+      .inputCustom::-webkit-input-placeholder {
+        color: #fff;
+        font-family: "Poppins-ExtraLight";
+      }
     }
+  }
 
-    .logoRow {
-        width: 250px;
-        margin: 0 auto;
-    }
+  .logoRow {
+    width: 250px;
+    margin: 0 auto;
+  }
 
-    .imgLogo {
-        position: fixed;
-        display: flex;
-        width: 150px;
-        top: 3%;
-        text-align: center;
-    }
+  .imgLogo {
+    position: fixed;
+    display: flex;
+    width: 150px;
+    top: 3%;
+    text-align: center;
+  }
 
-    .imgCustom {
-        top: -10%;
-        left: 35%;
-        position: fixed;
-        width: 85%;
-    }
+  .imgCustom {
+    top: -10%;
+    left: 35%;
+    position: fixed;
+    width: 85%;
+  }
 }
 </style>
