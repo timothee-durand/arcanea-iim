9352f1c32a61d431c5f84b1fac647bade47f29c2
Tristan Seclet <tristanseclet@gmail.com>
2023-02-22 20:29:28 +0100

    wip tristan

--- front/src/main.ts
+++ front/src/main.ts
@@ -7,6 +7,7 @@ import Toast from "vue-toastification";
 import "vue-toastification/dist/index.css";
 import {router} from "@/router";
 
+
 const app = createApp(App)
 app.use(router)
 app.use(createPinia())
