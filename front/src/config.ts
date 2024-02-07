fef9bf8955f2d72751b9642e0ca719ec1af8060f
Timothée DURAND <timothee.durand70@gmail.com>
2024-02-07 20:02:03 +0100

    chore: update to https

--- front/src/config.ts
+++ front/src/config.ts
@@ -1,1 +1,1 @@
-export const BASE_URL = import.meta.env.DEV ? "http://localhost:3000" : "http://api.arcanea.timotheedurand.fr"
+export const BASE_URL = import.meta.env.DEV ? "http://localhost:3000" : "https://api.arcanea.timotheedurand.fr"
